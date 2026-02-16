aa610fcf2778798630d1c8f7ab0288f23d857d01
IvanPostu <ipostu20000127@gmail.com>
2026-02-16 02:31:14 +0200

    chore: play with graphql

--- task-management-backend/entrypoint/src/main/java/com/iv127/task/management/backend/entrypoint/controller/DemoController.java
+++ task-management-backend/entrypoint/src/main/java/com/iv127/task/management/backend/entrypoint/controller/DemoController.java
@@ -1,14 +1,19 @@
 package com.iv127.task.management.backend.entrypoint.controller;
 
+import com.iv127.task.management.backend.entrypoint.graphql.FileInfo;
 import com.iv127.task.management.backend.entrypoint.model.Employee;
 import com.iv127.task.management.backend.entrypoint.model.Store;
 import com.iv127.task.management.backend.entrypoint.service.DemoService;
+import graphql.schema.DataFetchingEnvironment;
+import jakarta.servlet.http.Part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -39,6 +44,30 @@ public class DemoController {
 //
 //        return "aa11";
 //    }
+
+    @MutationMapping
+    public FileInfo singleUpload(Part file, DataFetchingEnvironment env) throws IOException {
+        // In some setups, use env.getArgument("file") if direct arg is null
+        if (file == null) {
+            file = env.getArgument("file");
+        }
+        if (file.getSize() == 0) {
+            throw new IllegalArgumentException("File is empty");
+        }
+
+        // Process the file (e.g., save to disk)
+        // file.getInputStream() for content; file.transferTo(...) if using MultipartFile cast
+        String filename = file.getSubmittedFileName();
+        long size = file.getSize();
+        String contentType = file.getContentType();
+
+        // Example: Save to /uploads/
+        // try (InputStream is = file.getInputStream()) {
+        //     Files.copy(is, Paths.get("/uploads/" + filename));
+        // }
+
+        return new FileInfo(filename, (int) size, contentType);
+    }
 
     @SchemaMapping
     public Store location(Employee employee) {
