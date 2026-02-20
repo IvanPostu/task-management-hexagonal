7bf071d2410f75c6682964e1efc1ea80ae493cad
IvanPostu <ipostu20000127@gmail.com>
2026-02-21 00:03:33 +0200

    test: refactor integration tests

---
+++ task-management-backend/entrypoint/src/test/java/com/iv127/task/management/backend/entrypoint/adapters/in/graphql/TaskGraphqlControllerRepositoryTest.java
@@ -0,0 +1,217 @@
+package com.iv127.task.management.backend.entrypoint.adapters.in.graphql;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+final class TaskGraphqlControllerRepositoryTest {
+
+    private TaskGraphqlControllerRepositoryTest() {
+    }
+
+    static void testCreateTask(WebApplicationContext context) {
+        String query = """
+                mutation CreateTask($input: CreateTaskDto!)
+                {
+                    createTask(input: $input) { id title description priority status dueDate createDate }
+                }
+                """;
+
+        WebTestClient.Builder webTestClientBuilder = MockMvcWebTestClient.bindToApplicationContext(context)
+                .configureClient()
+                .baseUrl("/graphql");
+
+        HttpGraphQlTester tester = HttpGraphQlTester
+                .builder(webTestClientBuilder)
+                .build();
+
+        Instant beforeCreatedDate = Instant.now();
+        TaskDto taskDto = tester.document(query)
+                .variable("input", new CreateTaskDto("title1", "description1", TaskPriorityDto.LOW, LocalDate.parse("2026-12-25")))
+                .execute()
+                .path("createTask")
+                .entity(TaskDto.class)
+                .get();
+
+        assertThat(taskDto.id()).isNotEmpty();
+        assertThat(taskDto.title()).isEqualTo("title1");
+        assertThat(taskDto.description()).isEqualTo("description1");
+        assertThat(taskDto.priority()).isEqualTo(TaskPriorityDto.LOW);
+        assertThat(taskDto.status()).isEqualTo(TaskStatusDto.CREATED);
+        assertThat(taskDto.dueDate()).isEqualTo(LocalDate.parse("2026-12-25"));
+        assertThat(taskDto.createDate())
+                .isAfter(beforeCreatedDate)
+                .isBefore(Instant.now());
+
+    }
+
+    static void testUpdateTask(WebApplicationContext context) {
+        String createTaskMutation = """
+                mutation CreateTask($input: CreateTaskDto!)
+                {
+                    createTask(input: $input) { id title description priority status dueDate createDate }
+                }
+                """;
+        String updateTaskMutation = """
+                mutation UpdateTask($input: UpdateTaskDto!) {
+                    updateTask(input: $input) { id title description priority status dueDate createDate }
+                }
+                """;
+
+        WebTestClient.Builder webTestClientBuilder = MockMvcWebTestClient.bindToApplicationContext(context)
+                .configureClient()
+                .baseUrl("/graphql");
+
+        HttpGraphQlTester tester = HttpGraphQlTester
+                .builder(webTestClientBuilder)
+                .build();
+
+        TaskDto createdTask = tester.document(createTaskMutation)
+                .variable("input", new CreateTaskDto("title1", "description1", TaskPriorityDto.LOW, LocalDate.parse("2026-12-25")))
+                .execute()
+                .path("createTask")
+                .entity(TaskDto.class)
+                .get();
+
+        TaskDto updatedTask = tester.document(updateTaskMutation)
+                .variable("input", new UpdateTaskDto(createdTask.id(), "title2", "description2", TaskPriorityDto.HIGH, TaskStatusDto.IN_PROGRESS, LocalDate.parse("2027-12-25")))
+                .execute()
+                .path("updateTask")
+                .entity(TaskDto.class)
+                .get();
+
+        assertThat(updatedTask.id()).isEqualTo(createdTask.id());
+        assertThat(updatedTask.title()).isEqualTo("title2");
+        assertThat(updatedTask.description()).isEqualTo("description2");
+        assertThat(updatedTask.priority()).isEqualTo(TaskPriorityDto.HIGH);
+        assertThat(updatedTask.status()).isEqualTo(TaskStatusDto.IN_PROGRESS);
+        assertThat(updatedTask.dueDate()).isEqualTo(LocalDate.parse("2027-12-25"));
+        assertThat(updatedTask.createDate()).isEqualTo(createdTask.createDate());
+    }
+
+    static void testGetTasksByFilter(WebApplicationContext context) {
+        String createTaskMutation = """
+                mutation CreateTask($input: CreateTaskDto!)
+                {
+                    createTask(input: $input) { id title description priority status dueDate createDate }
+                }
+                """;
+        String getTasksQuery = """
+                query TaskFilterInput($filter: TaskFilterInput!) {
+                    getTasksByFilter(filter: $filter) { id title description priority status dueDate createDate }
+                }
+                """;
+
+        WebTestClient.Builder webTestClientBuilder = MockMvcWebTestClient.bindToApplicationContext(context)
+                .configureClient()
+                .baseUrl("/graphql");
+
+        HttpGraphQlTester tester = HttpGraphQlTester
+                .builder(webTestClientBuilder)
+                .build();
+
+        String title = RandomStringUtils.insecure().nextAlphanumeric(32);
+        String description = RandomStringUtils.insecure().nextAlphanumeric(32);
+
+        TaskDto createdTask = tester.document(createTaskMutation)
+                .variable("input", new CreateTaskDto(title, description, TaskPriorityDto.LOW, LocalDate.parse("2026-12-25")))
+                .execute()
+                .path("createTask")
+                .entity(TaskDto.class)
+                .get();
+
+        List<TaskDto> tasks = tester.document(getTasksQuery)
+                .variable("filter", new TaskFilterInput(TaskPriorityDto.LOW, TaskStatusDto.CREATED, LocalDate.parse("2026-12-26")))
+                .execute()
+                .path("getTasksByFilter")
+                .entityList(TaskDto.class)
+                .get();
+
+        assertThat(tasks)
+                .anySatisfy(taskDto -> {
+                    assertThat(taskDto)
+                            .usingRecursiveComparison()
+                            .isEqualTo(createdTask);
+                });
+    }
+
+    static void testDeleteTask(WebApplicationContext context) {
+        String createTaskMutation = """
+                mutation CreateTask($input: CreateTaskDto!)
+                {
+                    createTask(input: $input) { id title description priority status dueDate createDate }
+                }
+                """;
+        String getTasksQuery = """
+                query TaskFilterInput($filter: TaskFilterInput!) {
+                    getTasksByFilter(filter: $filter) { id title description priority status dueDate createDate }
+                }
+                """;
+        String deleteTask = """
+                mutation DeleteTask($id: String!) { deleteTaskById(id: $id) }
+                """;
+
+        WebTestClient.Builder webTestClientBuilder = MockMvcWebTestClient.bindToApplicationContext(context)
+                .configureClient()
+                .baseUrl("/graphql");
+
+        HttpGraphQlTester tester = HttpGraphQlTester
+                .builder(webTestClientBuilder)
+                .build();
+
+        String title = RandomStringUtils.insecure().nextAlphanumeric(32);
+        String description = RandomStringUtils.insecure().nextAlphanumeric(32);
+
+        TaskDto createdTask = tester.document(createTaskMutation)
+                .variable("input", new CreateTaskDto(title, description, TaskPriorityDto.LOW, LocalDate.parse("2026-12-25")))
+                .execute()
+                .path("createTask")
+                .entity(TaskDto.class)
+                .get();
+
+        List<TaskDto> tasks = tester.document(getTasksQuery)
+                .variable("filter", new TaskFilterInput(TaskPriorityDto.LOW, TaskStatusDto.CREATED, LocalDate.parse("2026-12-26")))
+                .execute()
+                .path("getTasksByFilter")
+                .entityList(TaskDto.class)
+                .get();
+
+        assertThat(tasks)
+                .anySatisfy(taskDto -> {
+                    assertThat(taskDto)
+                            .usingRecursiveComparison()
+                            .isEqualTo(createdTask);
+                });
+
+        Boolean deleted = tester.document(deleteTask)
+                .variable("id", createdTask.id())
+                .execute()
+                .path("deleteTaskById")
+                .entity(Boolean.class)
+                .get();
+        assertThat(deleted).isTrue();
+
+        tasks = tester.document(getTasksQuery)
+                .variable("filter", new TaskFilterInput(TaskPriorityDto.LOW, TaskStatusDto.CREATED, LocalDate.parse("2026-12-26")))
+                .execute()
+                .path("getTasksByFilter")
+                .entityList(TaskDto.class)
+                .get();
+
+        assertThat(tasks)
+                .noneSatisfy(taskDto -> {
+                    assertThat(taskDto)
+                            .usingRecursiveComparison()
+                            .isEqualTo(createdTask);
+                });
+    }
+
+}
